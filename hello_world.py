9ec6905656758e84d04ca9b368fbe0cf136d7bf6
four1squad <87648612+four1squad@users.noreply.github.com>
2023-10-28 17:45:22 +0300

    Добавлена функция goodbye в hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
 def hello(text: str = "world"):
     print(f"Hello {text}!")
 
+def goodbye(text: str = "world"):
+    print(f"Goodbye {text}!")
+    
+
 if __name__ == "__main__":
     hello()
+    goodbye()
